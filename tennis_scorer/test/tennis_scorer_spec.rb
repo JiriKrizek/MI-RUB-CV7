0357135ca5ee7defcc739616461d705c610a29eb
Jiri Krizek <jkrizek@gmail.com>
2011-10-31 07:35:57 +0100

    Initial commit

---
+++ tennis_scorer/test/tennis_scorer_spec.rb
@@ -0,0 +1,11 @@
+require_relative "../lib/tennis_scorer"
+describe "TennisScorer", "basic scoring" do
+  it "should start with a score of 0-0" do
+    ts = TennisScorer.new
+    ts.score.should == "0-0"
+  end
+  it "should be 15-0 if the server wins a point"
+  it "should be 0-15 if the receiver wins a point"
+  it "should be 15-15 after they both win a point"
+  # ...
+end
