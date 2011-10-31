630e5a0efeb25cd9e07497454bbbfb21a56fc7e5
Jiri Krizek <jkrizek@gmail.com>
2011-10-31 08:52:32 +0100

    Adr. struktura

--- roman/test/test_roman.rb
+++ roman/test/test_roman.rb
@@ -1,12 +1,16 @@
-require_relative 'roman'
+require_relative '../lib/roman'
 require 'test/unit'
 class TestRoman < MiniTest::Unit::TestCase
   def test_simple
-    assert_equal("i", Roman.new(1).to_s)
     assert_equal("ii", Roman.new(2).to_s)
     assert_equal("iii", Roman.new(3).to_s)
     assert_equal("iv", Roman.new(4).to_s)
     assert_equal("ix", Roman.new(9).to_s)
+  end
+
+  def test_extremes
+    assert_equal("i", Roman.new(1).to_s)
+    assert_equal("mcmxcix", Roman.new(4999).to_s)
   end
 
   def test_init
