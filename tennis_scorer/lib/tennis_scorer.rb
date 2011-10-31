778c511e165acbce37be6e589d46fbc41bd2b092
Jiri Krizek <jkrizek@gmail.com>
2011-10-31 12:26:31 +0100

    RSpec oprava

--- tennis_scorer/lib/tennis_scorer.rb
+++ tennis_scorer/lib/tennis_scorer.rb
@@ -1,3 +1,41 @@
+class Score
+
+  attr_accessor :srv, :cl
+
+  def initialize
+    @srv = 0
+    @cl = 0
+  end
+
+  def to_s
+    "#{srv}-#{cl}"
+  end
+end
+
 class TennisScorer
-  
+  attr_accessor :score
+
+  def initialize
+    @score = Score.new
+  end
+
+  def score
+    @score
+  end
+
+  def cl_win
+    if @score.cl == 30
+      @score.cl = @score.cl + 10  
+    else
+      @score.cl = @score.cl + 15
+    end
+  end
+
+  def srv_win
+    if @score.srv == 30
+      @score.srv = @score.srv + 10 
+    else
+      @score.srv = @score.srv + 15
+    end
+  end
 end
--- tennis_scorer/test/tennis_scorer_spec.rb
+++ tennis_scorer/test/tennis_scorer_spec.rb
@@ -3,13 +3,13 @@ require_relative "../lib/tennis_scorer"
 describe "TennisScorer", "basic scoring" do
   it "should start with a score of 0-0" do
     ts = TennisScorer.new
-    ts.score.should == "0-0"
+    ts.score.to_s.should == "0-0"
   end
   
   it "should be 15-0 if the server wins a point" do
     ts = TennisScorer.new
     ts.srv_win
-    ts.score.should == "15-0"
+    ts.score.to_s.should eq("15-0")
   end
 
   it "should be 40-0 if the server wins 3 points" do
@@ -17,7 +17,7 @@ describe "TennisScorer", "basic scoring" do
     ts.srv_win
     ts.srv_win
     ts.srv_win
-    ts.score.should == "40-0"
+    ts.score.to_s.should eq("40-0")
   end
 
   it "should be 0-40 if the server wins 3 points" do
@@ -25,20 +25,20 @@ describe "TennisScorer", "basic scoring" do
     ts.cl_win
     ts.cl_win
     ts.cl_win
-    ts.score.should == "0-40"
+    ts.score.to_s.should eq("0-40")
   end
 
   it "should be 0-15 if the receiver wins a point" do
     ts = TennisScorer.new
     ts.cl_win
-    ts.score.should == "0-15"
+    ts.score.to_s.should eq("0-15")
   end
 
   it "should be 15-15 after they both win a point" do
     ts = TennisScorer.new
     ts.srv_win
     ts.cl_win
-    ts.score.should == "15-15"
+    ts.score.to_s.should eq("15-15")
   end
 end
 
